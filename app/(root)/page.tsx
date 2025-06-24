524cf1bddceaa5955f63bc853397c35513f389df
Kresna <dwkiller831@gmail.com>
2025-06-24 22:17:58 +0700

    2

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,23 @@
 
 import Image from "next/image";
 import Modal from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () =>  {
+
+  const isOpen = useStoreModal((state) => state.isOpen);
+  const onOpen = useStoreModal((state) => state.OnOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+  
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-gray-100">
-      <Modal isOpen onClose={() => {}} title="Welcome" description="This is a sample modal">
-        children
-      </Modal>
+        Root Page
     </div>
   );
 }
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -2,17 +2,71 @@
 
 import { useStoreModal } from "@/hooks/use-store-modal"
 import Modal from "../ui/modal"
+import * as z from 'zod'
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: 'Store name is required'
+    }),
+});
+
+
+
+const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // Handle form submission logic here
+}
 
 export const StoreModal = () => {
 
+    
     const storeModal = useStoreModal();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: '',
+    
+        }
+    });
+
     return (
         <Modal 
-        title="buat store"
+        title="Create store"
         description="Tambah store anda di sini"
         isOpen={storeModal.isOpen}
         onClose={storeModal.OnClose}
         >
-                Store Form
+        <div>
+            <div>
+                <Form {...form}>
+                    <form>
+                        <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Store Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Nama Toko" {...field}/>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}/>
+                        <div>
+                            <Button>
+                                Cancel
+                            </Button>
+                        </div>
+                    </form>
+                    
+                </Form>
+            </div>
+        </div>
         </Modal>)
 }
